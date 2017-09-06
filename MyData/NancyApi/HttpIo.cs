50c5963545139fc9192499484bd82110f7caa5a4
it-arjan <it-arjan@users.noreply.github.com>
2017-09-06 15:46:08 +0200

    use httpclient for both sync and async calls. Also handle multiuser with the static client

--- MyData/NancyApi/HttpIo.cs
+++ MyData/NancyApi/HttpIo.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
-using EasyHttp.Http;
 using NLogWrapper;
 using EasyHttp.Infrastructure;
 
@@ -17,67 +16,35 @@ namespace MyData.NancyApi
         private string _socketToken; // to identify which socket
         private static readonly NLogWrapper.ILogger _logger = LogManager.CreateLogger(typeof(NancyApiDb));
 
-        // static httpclients improve performance
-        private static System.Net.Http.HttpClient _httpClient = new System.Net.Http.HttpClient();
-        private static EasyHttp.Http.HttpClient _eHttp = new EasyHttp.Http.HttpClient();
+        // static httpclient reduces overhead
+        private static HttpClient _staticHttpClient = new HttpClient();
 
         public HttpIo(string apiToken, string socketServerAccessToken, string socketFeedId)
         {
             _apiToken = apiToken;
             _socketServerAccessToken = socketServerAccessToken;
             _socketToken = socketFeedId;
-
+            ResetSessionSpecificHeaders(_staticHttpClient);
         }
-        public string SyncRequest(System.Net.Http.HttpMethod method, string url, string json, string contentType)
+
+        public void ResetSessionSpecificHeaders(HttpClient httpClient)
         {
-            string result = string.Empty;
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _apiToken);
 
-            _eHttp.Request.AddExtraHeader("Authorization", string.Format("bearer {0}", _apiToken));
-            _eHttp.Request.AddExtraHeader("X-socketServerAccessToken", _socketServerAccessToken);
-            _eHttp.Request.AddExtraHeader("X-socketFeedId", _socketToken);
-            _eHttp.Request.Accept = contentType;
-            bool error = false;
-            if (method == System.Net.Http.HttpMethod.Get)
-            {
-                _eHttp.Get(url);
-                error = _eHttp.Response.StatusCode != System.Net.HttpStatusCode.OK;
-                if (!error)
-                    result = _eHttp.Response.RawText;
-            }
-            else if (method == System.Net.Http.HttpMethod.Post)
-            {
-                _eHttp.Post(url, json, contentType);
-                error = _eHttp.Response.StatusCode != System.Net.HttpStatusCode.OK;
-            }
-            else if (method == System.Net.Http.HttpMethod.Delete)
-            {
-                _eHttp.Delete(url, contentType);
-                error = _eHttp.Response.StatusCode != System.Net.HttpStatusCode.NoContent;
-            }
-            else
-            {
-                throw new HttpException(_eHttp.Response.StatusCode, _eHttp.Response.StatusDescription);
-            }
+            httpClient.DefaultRequestHeaders.Remove("X-socketServerAccessToken");
+            httpClient.DefaultRequestHeaders.Add("X-socketServerAccessToken", _socketServerAccessToken);
 
-            if (error)
-            {
-                _logger.Error(" {0} request returned {1}", method, _eHttp.Response.StatusCode);
-                throw new HttpException(_eHttp.Response.StatusCode, _eHttp.Response.StatusDescription);
-            }
-
-            return result;
+            httpClient.DefaultRequestHeaders.Remove("X-socketFeedId");
+            httpClient.DefaultRequestHeaders.Add("X-socketFeedId", _socketToken);
         }
 
-        public async Task<string> AsyncRequest(System.Net.Http.HttpMethod method, string url, string json, string contentType)
+        public string SyncRequest(System.Net.Http.HttpMethod method, string url, string json, string contentType)
         {
             HttpResponseMessage httpResponseMsg = null;
 
-            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _apiToken);
-            _httpClient.DefaultRequestHeaders.Accept.Add(
-                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(contentType)
-                );
-            _httpClient.DefaultRequestHeaders.Add("X-socketServerAccessToken", _socketServerAccessToken);
-            _httpClient.DefaultRequestHeaders.Add("X-socketFeedId", _socketToken);
+            var accept = new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(contentType);
+            if (!_staticHttpClient.DefaultRequestHeaders.Accept.Contains(accept))
+                _staticHttpClient.DefaultRequestHeaders.Accept.Add(accept);
 
             var ReponseMsg = string.Empty;
 
@@ -86,15 +53,60 @@ namespace MyData.NancyApi
             {
                 if (method == System.Net.Http.HttpMethod.Get)
                 {
-                    httpResponseMsg = await _httpClient.GetAsync(url);
+                    httpResponseMsg = _staticHttpClient.GetAsync(url).Result;
                 }
                 else if (method == System.Net.Http.HttpMethod.Post)
                 {
-                    httpResponseMsg = await _httpClient.PostAsync(url, new StringContent(json));
+                    httpResponseMsg = _staticHttpClient.PostAsync(url, new StringContent(json)).Result;
                 }
                 else if (method == System.Net.Http.HttpMethod.Delete)
                 {
-                    httpResponseMsg = await _httpClient.DeleteAsync(url);
+                    httpResponseMsg = _staticHttpClient.DeleteAsync(url).Result;
+                }
+                else
+                {
+                    throw new Exception("unkown http method");
+                }
+
+                resultStatus = httpResponseMsg.StatusCode;
+                var logMsg = string.Format("Log msg: {0} returned {1}", url, resultStatus);
+                _logger.Info(logMsg);
+
+                ReponseMsg = httpResponseMsg.Content.ReadAsStringAsync().Result;
+            }
+            catch (Exception ex)
+            {
+                ReponseMsg = ex.Message;
+                _logger.Error(ReponseMsg);
+            }
+
+            return ReponseMsg;
+        }
+
+        public async Task<string> AsyncRequest(System.Net.Http.HttpMethod method, string url, string json, string contentType)
+        {
+            HttpResponseMessage httpResponseMsg = null;
+
+            _staticHttpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(contentType)
+                );
+ 
+            var ReponseMsg = string.Empty;
+
+            var resultStatus = System.Net.HttpStatusCode.Ambiguous;
+            try
+            {
+                if (method == System.Net.Http.HttpMethod.Get)
+                {
+                    httpResponseMsg = await _staticHttpClient.GetAsync(url);
+                }
+                else if (method == System.Net.Http.HttpMethod.Post)
+                {
+                    httpResponseMsg = await _staticHttpClient.PostAsync(url, new StringContent(json));
+                }
+                else if (method == System.Net.Http.HttpMethod.Delete)
+                {
+                    httpResponseMsg = await _staticHttpClient.DeleteAsync(url);
                 }
                 else
                 {
